f90a1276b488c9a6bb0e58b27f90f160d1f8e031
Deepak9545 <dipakyadav1564@gmail.com>
2026-09-05 21:05:49 +0530

    project

---
+++ project_03/index.js
@@ -0,0 +1,31 @@
+
+// generate a random color
+const randomcolor = function(){
+    const hex= '0123456789ABCDEF';
+    let color='#';
+
+    for(let i=0; i<6; i++ ){
+        color += hex[Math.floor(Math.random()*16)];
+
+    }
+    return color;
+}
+
+const startchangingcolor= function(){
+   interval= setInterval( ()=>{
+       document.body.style.backgroundColor= randomcolor();
+     } ,1000)
+
+    
+}
+    
+
+
+
+const stopchangingcolor= function(){
+    clearInterval(interval)
+    interval= null;
+}
+document.querySelector("#start").addEventListener('click', startchangingcolor)
+
+document.querySelector("#stop").addEventListener('click', stopchangingcolor)
